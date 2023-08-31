02678684c998451fe00f235aa75d327e32faa4f4
NemiahHayes <hdx5116@autuni.ac.nz>
2023-08-31 12:33:43 +1200

    MainMenuFurtherUpdates

---
+++ HideAndSeek_V1.0/Assets/LoadScene.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+
+    private Button btn;
+	void Start () {
+		Button btn = GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
+	}
+
+	void TaskOnClick(){
+		SceneManager.LoadScene("Map_01", LoadSceneMode.Single);
+	}
+}
