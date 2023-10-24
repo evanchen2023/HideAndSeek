4b58e15e1b7b3b14a5c6a33101f9675227ff3b01
haitao pan <pht970218@gmail.com>
2023-10-24 19:59:39 +1300

    healthBar

--- HideAndSeek_V1.0/Assets/Scripts/Playerhealth.cs
+++ HideAndSeek_V1.0/Assets/Scripts/Playerhealth.cs
@@ -23,7 +23,7 @@ public class Playerhealth : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.F))
         {
             TakeDamage(20);
         }
